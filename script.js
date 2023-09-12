e11681fd04d6617dcf8566dedb833613d79c2cdf
rajesh appana <appanarajesh363@gmail.com>
2023-09-12 23:07:23 +0530

    final

--- script.js
+++ script.js
@@ -3,20 +3,51 @@ const quoteText=document.getElementById('quote');
 const quoteAuthor=document.getElementById('author');
 const twitterBtn=document.getElementById('twitter');
 const newQuoteBtn=document.getElementById('new-quote');
-
+const loader=document.getElementById('loader');
 
 let apiQuotes=[];
+
+//Show loading
+function loading(){
+    loader.hidden=false;
+    quotContainer.hidden=true;
+}
+
+//Hide loader
+function complete(){
+    quotContainer.hidden=false;
+    loader.hidden=true;
+}
+
 //Show Quote
 function newQuote(){
+    loading(); 
     //Pick a random quote from apiQuotes array
     const quote=apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    quoteAuthor.textContent=quote.author;
+    
+    //Check if author value is blank and replace empty author with 'Unknown'
+    if(!quote.author){
+        quoteAuthor.textContent='unknown'; 
+    }
+    else{
+        quoteAuthor.textContent=quote.author;
+    }
+    
+    // Check the quoe length to determine the CSS styling
+    if(quote.text.length>70){
+        quoteText.classList.add('long-quote');
+    }else {
+        quoteText.classList.remove('long-quote'); 
+    }
+    // set the quote , hide loading
     quoteText.textContent=quote.text;
-    console.log(quote);
+    complete();
+    // console.log(quote);
 }
 
 //Get Quotes from API
 async function getQuotes(){
+    loading();
     const apiurl='https://type.fit/api/quotes';
     try {
         const response=await fetch(apiurl);
@@ -28,5 +59,16 @@ async function getQuotes(){
     }
 }
 
+//Tweet Quote
+function tweetQuote(){
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+    window.open(twitterUrl,'_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 //On Load
 getQuotes();
+// loading();
